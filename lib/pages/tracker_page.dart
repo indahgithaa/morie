9ca2b455c5758235ff2539f08e48efbe6ac9058b
Indah Githa <indahgitha99@gmail.com>
2024-01-24 07:14:06 +0800

    import date picker librar, check its functionalities

--- lib/pages/tracker_page.dart
+++ lib/pages/tracker_page.dart
@@ -1,12 +1,99 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:intl/intl.dart';
+import 'package:calendar_date_picker2/calendar_date_picker2.dart';
 
-class TrackerPage extends StatelessWidget {
+class TrackerPage extends StatefulWidget {
+  const TrackerPage({super.key});
+
+  @override
+  State<TrackerPage> createState() => _TrackerPageState();
+}
+
+class _TrackerPageState extends State<TrackerPage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    print(DateFormat('E, MMM d').format(DateTime.parse("2024-01-23")));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Tracker Page"),
+      body: Column(
+        children: [
+          _buildDefaultSingleDatePickerWithValue(),
+        ],
       ),
     );
+  }
+
+  Widget _buildDefaultSingleDatePickerWithValue() {
+    final config = CalendarDatePicker2Config();
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const SizedBox(height: 10),
+        // const Text('Single Date Picker (With default value)'),
+        CalendarDatePicker2(
+          config: config,
+          value: _singleDatePickerValueWithDefaultValue,
+          onValueChanged: (dates) =>
+              setState(() => _singleDatePickerValueWithDefaultValue = dates),
+        ),
+        const SizedBox(height: 10),
+        Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            // const Text('Selection(s):  '),
+            const SizedBox(width: 10),
+            Text(
+              _getValueText(
+                config.calendarType,
+                _singleDatePickerValueWithDefaultValue,
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(height: 25),
+      ],
+    );
+  }
+
+  List<DateTime?> _singleDatePickerValueWithDefaultValue = [
+    DateTime.now(),
+  ];
+  String _getValueText(
+    CalendarDatePicker2Type datePickerType,
+    List<DateTime?> values,
+  ) {
+    values =
+        values.map((e) => e != null ? DateUtils.dateOnly(e) : null).toList();
+    var valueText = (values.isNotEmpty ? values[0] : null)
+        .toString()
+        .replaceAll('00:00:00.000', '');
+
+    if (datePickerType == CalendarDatePicker2Type.multi) {
+      valueText = values.isNotEmpty
+          ? values
+              .map((v) => v.toString().replaceAll('00:00:00.000', ''))
+              .join(', ')
+          : 'null';
+    } else if (datePickerType == CalendarDatePicker2Type.range) {
+      if (values.isNotEmpty) {
+        final startDate = values[0].toString().replaceAll('00:00:00.000', '');
+        final endDate = values.length > 1
+            ? values[1].toString().replaceAll('00:00:00.000', '')
+            : 'null';
+        valueText = '$startDate to $endDate';
+      } else {
+        return 'null';
+      }
+    }
+
+    return valueText;
   }
 }
